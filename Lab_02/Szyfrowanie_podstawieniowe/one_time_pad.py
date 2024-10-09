4de0c0bf20196d596d516c5ee2b13a8459ccb3b8
Rassena <gadzinski.michal@gmail.com>
2024-10-09 20:32:39 +0200

    commit

--- Lab_02/Szyfrowanie_podstawieniowe/one_time_pad.py
+++ Lab_02/Szyfrowanie_podstawieniowe/one_time_pad.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
 
+
+ASCII_MOD=128
+
 def read_file(file: Path) -> str:
-    with open(file, encoding="utf-8") as f:
+    with open(file, encoding="ASCII") as f:
         lines = f.readlines()
-        return "\n".join(lines)
+        return ''.join(lines)
 
 
 def write_file(file: Path, text: str):
-    with open(file, "w", encoding="utf-8") as f:
+    with open(file, "w", encoding="ASCII") as f:
         f.write(text)
 
 
@@ -20,6 +23,7 @@ def main():
     """Main program"""
     text = read_file("./plain.txt")
     write_file("./substitute_proprietary.txt", text)
+    print(text.encode())
     return 0
 
 
